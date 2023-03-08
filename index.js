c9aea20ed33f88ac6daded0931e970678459ecd3
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-03-07 20:59:18 -0500

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name)
+{
+    cats.push(name)
+}
+
+function  destructivelyPrependCat(name)
+{
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name)
+{
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name)
+{
+    cats.shift(name)
+}
+function appendCat(name)
+{   const AddCats=[...cats]
+    AddCats.push(name)
+    return AddCats;
+
+}
+
+function prependCat(name)
+{   const AddCats=[name, ...cats]
+    
+    return AddCats;
+
+}
+
+function removeLastCat()
+{
+   const RemoveCat = [...cats];
+   RemoveCat.pop();
+   return RemoveCat;
+
+
+}
+
+function  removeFirstCat()
+{
+   const RemoveCat = [...cats];
+   RemoveCat.shift();
+   return RemoveCat;
+
+
+}
